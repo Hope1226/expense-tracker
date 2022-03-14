fb99450157570a4712e79c2db9580c24ef79a3f8
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-14 22:58:21 +0500

    Generate categories ctr

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "categories#index"
+  
+  resources :categories, only: %i[index, show]
 
   # Defines the root path route ("/")
   # root "articles#index"
