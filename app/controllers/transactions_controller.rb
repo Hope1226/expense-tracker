a8bb9f1c5c7c2cdb08a6c3e4c7e4336501029ec0
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-16 04:05:05 +0500

    Add CRUD actions for transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,32 @@
 class TransactionsController < ApplicationController
-  def index; end
+  def show
+    @transaction = Entity.find(params[:id])
+  end
 
-  def show; end
+  def new
+    Entity.new
+  end
+
+  def create
+    @new_entity = Entity.new(entity_params)
+
+    if @new_entity.save
+      redirect_to category_path(id: params[:category_id]), notice: 'Spending successfully added'
+    else
+      render :new, alert: 'Oops, Something went wrong'
+    end
+  end
+
+  def destroy
+    Entity.find(params[:id]).delete
+    respond_to do |format|
+      format.html { redirect_to category_path(id: params[:category_id], notice: 'Category successfully removed') }
+    end
+  end
+
+  private
+
+  def entity_params
+    params.permit(:name, :amount, :user_id, :category_id)
+  end
 end
