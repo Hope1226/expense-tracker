d4c4e4cf3cb7e153c8f9c85a7f34442a6b8fab0a
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-15 19:52:41 +0500

    Implement CRUD actions for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,38 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
-  def show; end
+  def index
+    @categories = Group.all.where(user_id: current_uder.id);
+  end
+
+  def show
+    @category = Group.find(params[:id])
+  end
+
+  def new
+    Group.new
+  end
+
+  def create
+    @new_category = Group.new(group_params)
+
+    if @new_category.save
+      redirect_to :index, notice: 'Category successfully created'
+    else
+      render :new, alert: 'Oops, Something went wrong'
+    end
+  end
+
+  def destroy
+    Group.find(params[:id]).delete
+    respond_to do |format|
+      format.html { redirect_to :index, notice: 'Category successfully removed' }
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "categories#index"
   
-  resources :categories, only: %i[index show] do 
+  resources :categories, only: %i[index show new create destroy] do 
     resources :transactions, only: %i[index show]
   end
 
