4a9b535e7cde8f6567cb3213fbefad413db9dd00
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-17 21:38:01 +0500

    Finalize styling and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "categories#index"
+  root to: "home#index"
   
   resources :categories do 
     resources :transactions
   end
 
   resources :users, only: %i[show]
+  resources :home, only: %i[index]
 
   delete '/transactions', to: 'transactions#destroy_all'
 
